5d3d44ff9f066558937f2e682669a9264acf5c79
andrhome <homandrej@gmail.com>
2017-09-13 18:12:29 +0300

    Added JS with first functions

---
+++ js/app.js
@@ -0,0 +1,63 @@
+function createTodoItem(title) {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+
+    const label = document.createElement('label');
+    label.innerText = title;
+    label.className = 'title';
+
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = 'textfield';
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.className = 'edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.className = 'delete';
+
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+
+    bindEvents(listItem);
+
+    return listItem;
+}
+
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+}
+
+function addTodoItem(event) {
+    event.preventDefault();
+
+    if(addInput.value === '') return alert('You should type name of task!');
+
+    const listItem = createTodoItem(addInput.value);
+
+    todoList.appendChild(listItem);
+    addInput.value = '';
+}
+
+const todoForm = document.getElementById('todo-form');
+const addInput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
+
+todoForm.addEventListener('submit', addTodoItem);
